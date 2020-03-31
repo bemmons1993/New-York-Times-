8e75a43cf8e4230dcfe4d2150c5650a107a1d410
Jacob Zirbel <jzerble@gmail.com>
2020-03-30 21:24:14 -0500

    add function to then to display article info

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-var apiKey = "b0463859-e365-4f67-a0c8-3d02e6d75a00";
-
+var apiKey = "vUFjq3MoZWkNyu7gGXylgZHRyU6QsQdH";
+var mainDiv = $("#main-div");
 $("button").on("click", function() {
-	var searchTerms = "";
+	var searchTerms = "COVID";
 	var num = 1;
 	var queryURL =
 		"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
 		searchTerms +
 		"&api-key=" +
 		apiKey;
-
-	$.ajax({ url: queryURL, method: "GET" }).then(function(response) {});
+	$.ajax({ url: queryURL, method: "GET" }).then(function(response) {
+		console.log(response);
+		var newDiv = $("<div>");
+		var abstract = $("<p>").text(response.response.docs[0].abstract);
+		var url = $("<p>").text(response.response.docs[0].web_url);
+		var leadParagraph = $("<p>").text(response.response.docs[0].lead_paragraph);
+		newDiv.append(abstract, url, leadParagraph);
+		mainDiv.prepend(newDiv);
+	});
 });
