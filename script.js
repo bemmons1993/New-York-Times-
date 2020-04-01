6551528ff785b7e99266e89d166c17edac4a62bd
Jacob Zirbel <jzerble@gmail.com>
2020-04-01 17:13:31 -0500

    display results under form

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var apiKey = "vUFjq3MoZWkNyu7gGXylgZHRyU6QsQdH";
-var mainDiv = $("#main-div");
-$("button").on("click", function() {
-	var searchTerms = "COVID";
+var mainDiv = $(".results");
+$("button").on("click", function(event) {
+	event.preventDefault();
+	var searchTerms = document.getElementById("search-term").value;
+	console.log(searchTerms);
 	var num = 1;
 	var queryURL =
 		"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
@@ -10,11 +12,15 @@ $("button").on("click", function() {
 		apiKey;
 	$.ajax({ url: queryURL, method: "GET" }).then(function(response) {
 		console.log(response);
-		var newDiv = $("<div>");
-		var abstract = $("<p>").text(response.response.docs[0].abstract);
-		var url = $("<p>").text(response.response.docs[0].web_url);
-		var leadParagraph = $("<p>").text(response.response.docs[0].lead_paragraph);
-		newDiv.append(abstract, url, leadParagraph);
-		mainDiv.prepend(newDiv);
+		response.response.docs.forEach(element => {
+			var newDiv = $("<div>");
+			var abstract = $("<p>").text("Headline: " + element.headline.main);
+			var url = $("<p>").text("URL: " + element.web_url);
+			var leadParagraph = $("<p>").text(
+				"Lead Paragraph: " + element.lead_paragraph
+			);
+			newDiv.append(abstract, url, leadParagraph);
+			mainDiv.prepend(newDiv);
+		});
 	});
-}); 
+});
