af4c2b8f588b0ba62261208cc4e1e81e34932978
Jacob Zirbel <jzerble@gmail.com>
2020-04-01 17:53:46 -0500

    add search by year to api

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ var mainDiv = $(".results");
 $("button").on("click", function(event) {
 	event.preventDefault();
 	var searchTerms = document.getElementById("search-term").value;
-	console.log(searchTerms);
-	var num = 1;
-	var queryURL =
-		"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-		searchTerms +
-		"&api-key=" +
-		apiKey;
+	var num = document.getElementById("num-records-select").value;
+	var startYear = document.getElementById("start-year");
+	var endYear = document.getElementById("end-year");
+	var start, end;
+	if (startYear.value.match(/^\d{4}$/g)) {
+		start = startYear.value + "0101";
+	} else {
+		start = "12000101";
+		startYear.value = startYear.value ? "INVALID" : "";
+	}
+	if (endYear.value.match(/^\d{4}$/g)) {
+		end = endYear.value + "0101";
+	} else {
+		end = "99991231";
+		endYear.value = endYear.value ? "INVALID" : "";
+	}
+	var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerms}&begin_date=${start}&end_date=${end}?&api-key=${apiKey}`;
 	$.ajax({ url: queryURL, method: "GET" }).then(function(response) {
 		console.log(response);
 		response.response.docs.forEach(element => {
